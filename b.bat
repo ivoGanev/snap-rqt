a2d03b5c86195a8a578307137352c3599c94dbc4
Ivo Ganev <ivo.ganev@yahoo.com>
2025-02-08 17:36:56 +0000

    Ignore builds

--- b.bat
+++ b.bat
@@ -1,2 +1,12 @@
-go build
-snap-rq.exe
+@echo off
+set GOOS=windows
+set GOARCH=amd64
+
+:: Ensure the build directory exists
+if not exist build mkdir build
+
+:: Build the Go executable
+go build -o build\snap-rq.exe
+
+:: Run the executable
+build\snap-rq.exe
